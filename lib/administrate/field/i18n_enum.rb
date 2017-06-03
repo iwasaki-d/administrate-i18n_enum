b138ac28c79bafcfb04362099686f02fb9dec539
iwasaki_d <d.iwasaki.g@gmail.com>
2017-06-03 09:36:42 +0900

    Refactoring

--- lib/administrate/field/i18n_enum.rb
+++ lib/administrate/field/i18n_enum.rb
@@ -4,23 +4,31 @@ require 'rails'
 module Administrate
   module Field
     class I18nEnum < Administrate::Field::Select
-      class Engine < ::Rails::Engine; end
+      class Engine < ::Rails::Engine
+      end
 
       def enum_options
         enums = collection.map do |k, v|
-          [I18n.t(i18n_path(class_name, @attribute, k), default: k), k]
+          [translate(class_name, @attribute, k), k]
         end
-        enums.unshift(['', nil]) if @options[:null] == true
+        enums.unshift(['', nil]) if @options.fetch(:null, false) == true
+        enums
       end
+
+      private
 
       def class_name
-        @options[:class_name].downcase
+        @options.fetch(:class_name, '').camelcase.downcase
       end
 
-      def i18n_path(class_name, attribute, value)
-        i18n_path = "#{class_name}.#{attribute}.#{value}"
-        i18n_path = "#{@options[:scope]}." + i18n_path unless @options[:scope].nil?
-        i18n_path
+      def translate(class_name, attribute, value)
+        I18n.t("#{class_name}.#{attribute}.#{value}", scope: @options.fetch(:scope, nil), default: value)
+      end
+
+      def collection
+        super
+        method_name = @attribute.to_s.pluralize
+        @collection = @options.fetch(:class_name, '').constantize.send(method_name) if  @collection.empty?
       end
     end
   end
